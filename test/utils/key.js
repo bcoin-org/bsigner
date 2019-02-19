490fc7c1a391b91903877c0c6b8445c98d54e508
Mark Tyneway <mark@purse.io>
2019-02-18 19:44:45 -0800

    bugfix: turn hdprivkey to public

--- test/utils/key.js
+++ test/utils/key.js
@@ -17,7 +17,9 @@ const phrase = [
 ].join(' ');
 
 // TODO: assert on network type being valid
-function testxpub(index = 0, network = 'regtest') {
+function testxpub(index = 0, network) {
+  if (typeof network === 'string')
+    network = Network.get(network);
   const mnemonic = Mnemonic.fromPhrase(phrase);
 
   // m'
@@ -26,21 +28,16 @@ function testxpub(index = 0, network = 'regtest') {
   // m'/44'
   const bip44Key = priv.derive(44, true);
 
-  // technically the network should influece
-  // the derivation index here, but only
-  // the prefix really matters for encoding
-  // which network
-  // m'/44'/0'
-  const bitcoinKey = bip44Key.derive(0, true);
+  // m'/44'/{0,1}'
+  const coinType = network.keyPrefix.coinType;
+  const bitcoinKey = bip44Key.derive(coinType, true);
 
-  // m'/44'/0'/0'
+  // m'/44'/0'/${index}'
   const accountKey = bitcoinKey.derive(index, true);
 
-  // account extended public key
-  // https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki#Serialization_format
-  const xpub = accountKey.xpubkey(network);
-
-  return xpub;
+  // turn to public key
+  return accountKey.toPublic();
 }
 
 exports.testxpub = testxpub;
+exports.phrase = phrase;
