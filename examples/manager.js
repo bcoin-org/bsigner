9d90b8edce1a2aa46699eeb4bd809e0f9b610623
Nodar Chkuaselidze <nodar.chkuaselidze@gmail.com>
2019-12-27 11:25:23 +0400

    device: introduce trezor device.

--- examples/manager.js
+++ examples/manager.js
@@ -6,82 +6,48 @@
 'use strict';
 
 const LedgerDeviceManager = require('../lib/device/ledgermanager');
+const TrezorDeviceManager = require('../lib/device/trezormanager');
 const DeviceManager = require('../lib/device/manager');
 const { vendors } = require('../lib/common');
 const Logger = require('blgr');
 const Network = require('bcoin/lib/protocol/network');
 
-// Example 1.
-// Using LedgerDeviceManager directly.
-const runLedgerDeviceManager = async (logger, network) => {
-  const ledgerDeviceManager = new LedgerDeviceManager({ network, logger });
+// 'TREZOR' | 'LEDGER' | 'GENERIC'
+const MANAGER = (process.env.MANAGER || 'GENERIC').toUpperCase();
+const VENDOR = (process.env.VENDOR || 'ANY').toUpperCase();
 
-  ledgerDeviceManager.on('select', (device) => {
-    console.log('Select', device.key, device.handle);
-  });
-
-  ledgerDeviceManager.on('deselect', (device) => {
-    console.log('Deselect', device.key, device.handle);
-  });
-
-  ledgerDeviceManager.on('connect', (device) => {
-    console.log('Connect:', device.key, device.handle);
-
-    ledgerDeviceManager.selectDevice(device);
-  });
-
-  ledgerDeviceManager.on('disconnect', (device) => {
-    console.log('Disconnect:', device.key, device.handle);
-  });
-
-  await ledgerDeviceManager.open();
-  await ledgerDeviceManager.selectDevice();
-
-  const pubkey = await ledgerDeviceManager.getPublicKey('m/44\'/1\'/0\'');
-  console.log('Public Key: ', pubkey.xpubkey(network));
-
-  await ledgerDeviceManager.close();
-};
-
-// Example 2.
-// Using generic DeviceManager.
-const runDeviceManager = async (logger, network) => {
-  const deviceManager = new DeviceManager({ network, logger });
-
-  deviceManager.on('select', (device) => {
+const runDeviceManager = async (manager, network) => {
+  manager.on('select', (device) => {
     console.log('Select', device.vendor, device.key, device.handle);
   });
 
-  deviceManager.on('deselect', (device) => {
+  manager.on('deselect', (device) => {
     console.log('Deselect', device.vendor, device.key, device.handle);
   });
 
-  deviceManager.on('connect', (device) => {
+  manager.on('connect', (device) => {
     console.log('Connect:', device.vendor, device.key, device.handle);
 
-    // We could do two things:
-    //  1. overload first parameter to accept STRING or Device type and
-    //    act accordingly.
-    //  2. Ignore first parameter if device is present and use vendor
-    //    from the device (device.vendor)
-    //  3. Check vendor === device.vendor and throw if they don't match
-    //    but there are no benefits with this, you will just use
-    //    selectDevice(device.vendor, device);
-
-    deviceManager.selectDevice(device.vendor, device);
+    manager.selectDevice(device);
   });
 
-  deviceManager.on('disconnect', (device) => {
+  manager.on('disconnect', (device) => {
     console.log('Disconnect:', device.vendor, device.key, device.handle);
   });
 
-  await deviceManager.open();
-  await deviceManager.selectDevice(vendors.LEDGER);
+  await manager.open();
 
-  const pubkey = await deviceManager.getPublicKey('m/44\'/1\'/0\'');
+  if (MANAGER === 'GENERIC')
+    await manager.selectDevice(VENDOR === 'ANY' ? 'LEDGER' : VENDOR);
+  else
+    await manager.selectDevice();
+
+  const pubkey = await manager.getPublicKey('m/44\'/1\'/0\'');
   console.log('Public Key: ', pubkey.xpubkey(network));
+  const pubkey1 = await manager.getPublicKey('m/44\'/1\'/1\'');
+  console.log('Public Key: ', pubkey1.xpubkey(network));
 
-  await deviceManager.close();
+  await manager.close();
 };
 
 (async () => {
@@ -89,11 +55,27 @@ const runDeviceManager = async (logger, network) => {
   const logger = new Logger('spam');
   await logger.open();
 
-  console.log('-- ledger device manager --');
-  await runLedgerDeviceManager(logger, network);
-  console.log('-- generic device manager --');
-  await runDeviceManager(logger, network);
+  let manager;
+  switch (MANAGER) {
+    case vendors.LEDGER: {
+      manager = new LedgerDeviceManager({ network, logger });
+      break;
+    }
+    case vendors.TREZOR: {
+      manager = new TrezorDeviceManager({ network, logger });
+      break;
+    }
+    case 'GENERIC': {
+      const vendor = VENDOR;
+      manager = new DeviceManager({ network, logger, vendor });
+      break;
+    }
+    default: {
+      throw new Error(`Can not use ${MANAGER} as manager.`);
+    }
+  }
 
+  await runDeviceManager(manager, network);
   await logger.close();
 })().catch((err) => {
   console.error(err);
